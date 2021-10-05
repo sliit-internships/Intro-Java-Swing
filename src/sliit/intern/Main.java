3fb7ec2d6b6aba79cffca44080ba68f34d3c294c
Shyam Reyal <shyam3001@gmail.com>
2021-10-05 10:21:45 +0530

    changed hello world

--- src/sliit/intern/Main.java
+++ src/sliit/intern/Main.java
@@ -2,6 +2,6 @@ package sliit.intern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Introduction to Java Swing");
     }
 }
