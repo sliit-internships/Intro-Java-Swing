40c7b98377be101daa5d477aecb97480b0d72734
Shyam Reyal <shyam3001@gmail.com>
2021-10-05 10:46:33 +0530

    added dialog box to code

--- src/sliit/intern/MainFrame.java
+++ src/sliit/intern/MainFrame.java
@@ -4,6 +4,8 @@
  */
 package sliit.intern;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author shyam.r
@@ -55,6 +57,11 @@ public class MainFrame extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 0, 0));
         jButton1.setText("Click Me");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("Enter Your Name");
 
@@ -164,6 +171,12 @@ public class MainFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        JOptionPane.showMessageDialog(this, "Button was Clicked");
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
