d52ab9c3229ec62cd59633c902262ba27e56ea10
Shyam Reyal <shyam3001@gmail.com>
2021-10-05 10:29:29 +0530

    added first frame

--- src/sliit/intern/Main.java
+++ src/sliit/intern/Main.java
@@ -2,6 +2,7 @@ package sliit.intern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Introduction to Java Swing");
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
     }
 }
